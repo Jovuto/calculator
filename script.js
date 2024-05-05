eb59a2a19a575ec2a4229be8075c9b1ee8c4c147
Jovuto <153099429+Jovuto@users.noreply.github.com>
2024-05-05 10:41:12 +0100

    feat: add multiply function

--- script.js
+++ script.js
@@ -6,10 +6,18 @@ function add (...numbers) {
     return sum;
 }
 
-function sub (...numbers) {
+function subtract (...numbers) {
     let subArr = [];
     for (number of numbers){
         subArr.push(number);
     }
     return subArr.reduce((sum, currentNum) => sum - currentNum);
+}
+
+function multiply (...numbers) {
+    let multArr = [];
+    for (number of numbers){
+        multArr.push(number);
+    }
+    return multArr.reduce((sum, currentNum) => sum * currentNum);
 }
